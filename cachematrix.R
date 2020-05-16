382164199378a96e7f727e3330906cf3b26b4b1c
tlminhkhoa <40111341+tlminhkhoa@users.noreply.github.com>
2020-05-15 22:34:18 -0400

    Update cachematrix.R
    Add additional comment to the file

--- cachematrix.R
+++ cachematrix.R
@@ -5,17 +5,18 @@
 
 makeCacheMatrix <- function(x = matrix()) {
         inverseM <- NULL
+#         function to set x
         set <- function(y){
                 x <<- y
                 inverseM <<- NULL
         }
+#       function to get x
         get <- function() { x }
-        
+#       function to set the inverse 
         setInverse <- function(inverseArg){
-                print("here")
                 inverseM <<- inverseArg
         }
-        
+#       function to get the inverse
         getInverse <- function(){inverseM }
         
         return(list(set = set, get = get ,setInverse = setInverse, getInverse = getInverse))
@@ -23,7 +24,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
-
+# The function first check if the inverse exist or not if yes it return it
+# If the inverse is not there, it will calculate and store the inverse
+# then return it to the user
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inverseM <- makeCacheMatrix(x)$getInverse()
